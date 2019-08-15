140707c8ffae0f2d68afbb1a9fd31eaa1a7697b1
Serhii Kovalov <Serg.kovalyov81@gmail.com>
2019-08-15 13:26:14 +0300

    Commit15r

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,40 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code heres
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
+
+       /* int [] a = {12, 23, 1, 3};
+        int[] b = new int[5];
+        for (int i = 0; i <a.length-1; i++) {
+            System.out.println(a[i]);
+        }
+        for (int i : a) {
+            System.out.println("b = " + i);
+        }*/
+       /* int[][] c = {{1, 2, 3}, {5, 6, 7}};
+        System.out.println(c[0][1]);*/
+
+       /* for (int i = 0; i < 10; i++) {
+            if (i%2==0)return;
+            System.out.println("i = " + i);
+        }*/
+       /*String strString = "123";
+        String strDouble = "123.456";
+        int x; double y;
+        x = Integer.parseInt(strString);
+        y = Double.parseDouble(strDouble);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        strString = String.valueOf(x+1);
+        System.out.println("strString = " + strString);
+        strDouble = String.valueOf(y - 1);
+        System.out.println("strDouble = " + strDouble);
+        String str;
+        str = "num " + 342;
+        System.out.println("str = " + str);
+        // Приведение строчного к цифровому и обратно
+*/
+
 
     }
 }
+
