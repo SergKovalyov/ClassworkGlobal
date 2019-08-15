7f8061ee8fb177c5bdae08c60fe9a0b757742226
Serhii Kovalov <Serg.kovalyov81@gmail.com>
2019-08-15 12:41:56 +0300

    Commit15reset

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,10 @@ public class Main {
         System.out.println("HI");
         System.out.println("HI");
         System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        yiygigyigyi
     }
 }
